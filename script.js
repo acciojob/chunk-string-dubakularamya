0dd11d9888aaedca35a19f57fa4d68d79d5d9289
dubakularamya <150138715+dubakularamya@users.noreply.github.com>
2025-05-12 10:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ function stringChop(str, size) {
 // Do not change the code below
 //const str = prompt("Enter String.");
 //const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+//alert(stringChop(str, size));
